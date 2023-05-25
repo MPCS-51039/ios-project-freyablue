96360cb97e5d531a5c242ebc95769f66475311af
yijia <yijiac@uchicago.edu>
2023-05-25 01:57:04 -0500

    Detail Page Debug

--- AngryBird/AngryBird/Controllers/DetailViewController.swift
+++ AngryBird/AngryBird/Controllers/DetailViewController.swift
@@ -10,6 +10,7 @@ import MapKit
 
 class DetailViewController: UIViewController {
 
+    @IBOutlet weak var whaleImageView: UIImageView!
     @IBOutlet weak var whaleLabel: UILabel!
     
     @IBOutlet weak var descriptionLabel: UILabel!
@@ -22,14 +23,29 @@ class DetailViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        self.whaleLabel.text = "Orca"
-        self.descriptionLabel.text = "Killer whale"
+        self.whaleLabel.text = bird.name
+        self.descriptionLabel.text = bird.description
         self.habitatLabel.text = "From Arctic"
         // Set initial location in Honolulu
         let initialLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
         map.centerToLocation(initialLocation)
+        
+        DispatchQueue.global(qos:
+            .userInitiated).async {
+                let colorImageData = NSData(contentsOf:
+                    URL(string: self.bird!.imageURL)!)
+                DispatchQueue.main.async {
+                    self.whaleImageView.image = UIImage(data: colorImageData as! Data)
+                    self.whaleImageView.layer.cornerRadius = self.whaleImageView.frame.width / 2
+                }
+                               
+            }
+                       
     }
     
+    // DispatchQueue
+    
+
     
     /*
     // MARK: - Navigation
